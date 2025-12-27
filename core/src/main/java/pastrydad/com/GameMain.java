f39a0c1adb18964ff6db216ffe37fb2cd0efb525
raynlance <103376466+Raynlance@users.noreply.github.com>
2025-12-27 23:05:20 +0100

    first working folder structure SAVE 1

---
+++ core/src/main/java/pastrydad/com/GameMain.java
@@ -0,0 +1,16 @@
+package pastrydad.com;
+
+import com.badlogic.gdx.Game;
+
+public class GameMain extends Game {
+
+    @Override
+    public void create() {
+        // Later: setScreen(new MenuScreen(this));
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
